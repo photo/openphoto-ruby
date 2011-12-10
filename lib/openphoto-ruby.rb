90c2bf2bca4876f6e329131a7c4f554d01ae9e80
Beans0063 <beans@Beanserver.net>
2011-12-10 00:23:30 -0800

    First commit
    Project scaffold via bundler

---
+++ lib/openphoto-ruby.rb
@@ -0,0 +1,7 @@
+require "openphoto-ruby/version"
+
+module Openphoto
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/openphoto-ruby/version.rb
@@ -0,0 +1,5 @@
+module Openphoto
+  module Ruby
+    VERSION = "0.0.1"
+  end
+end
